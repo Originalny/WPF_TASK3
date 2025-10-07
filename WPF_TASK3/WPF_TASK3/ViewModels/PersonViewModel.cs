16494ad09458095ba46400b3ceb106397df44f4b
Soma <39415978+Originalny@users.noreply.github.com>
2025-10-07 09:35:08 +0000

    Add files via upload

---
+++ WPF_TASK3/WPF_TASK3/ViewModels/PersonViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF_TASK3.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name 
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        
+        protected void OnPropertyChanged([CallerMemberName] string ? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
